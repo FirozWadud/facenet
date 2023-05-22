a90826340a2be113e5b76a8122d23d40164a1c9e
FirozWadud <mdfirozwadud.adm@gmail.com>
2023-05-22 16:52:32 +0600

    first commit

--- faceDetection.py
+++ faceDetection.py
@@ -1,58 +1,42 @@
 import cv2
 import typing
 import numpy as np
-import mediapipe as mp
+from mtcnn import MTCNN
 
-class MPFaceDetection:
-
+class MTCNNFaceDetection:
     def __init__(
         self,
-        model_selection: bool = 1,
-        confidence: float = 0.5,
-        mp_drawing_utils: bool = True,
+        min_confidence: float = 0.5,
+        draw_detections: bool = True,
         color: typing.Tuple[int, int, int] = (255, 255, 255),
         thickness: int = 2,
-        ) -> None:
-
-        self.mp_drawing_utils = mp_drawing_utils
+    ) -> None:
+        self.min_confidence = min_confidence
+        self.draw_detections = draw_detections
         self.color = color
         self.thickness = thickness
-        self.mp_drawing = mp.solutions.drawing_utils
-        self.mp_face_detection = mp.solutions.face_detection
-        self.face_detection = self.mp_face_detection.FaceDetection(model_selection=model_selection, min_detection_confidence=confidence)
+        self.detector = MTCNN()
 
-    def tlbr(self, frame: np.ndarray, mp_detections: typing.List) -> np.ndarray:
-
+    def tlbr(self, frame: np.ndarray, mtcnn_detections: typing.List) -> np.ndarray:
         detections = []
-        frame_height, frame_width, _ = frame.shape
-        for detection in mp_detections:
-            height = int(detection.location_data.relative_bounding_box.height * frame_height)
-            width = int(detection.location_data.relative_bounding_box.width * frame_width)
-            left = max(0 ,int(detection.location_data.relative_bounding_box.xmin * frame_width))
-            top = max(0 ,int(detection.location_data.relative_bounding_box.ymin * frame_height))
-
-            detections.append([top, left, top + height, left + width])
+        for detection in mtcnn_detections:
+            x, y, width, height = detection['box']
+            left, top, right, bottom = x, y, x + width, y + height
+            detections.append([top, left, bottom, right])
 
         return np.array(detections)
 
     def __call__(self, frame: np.ndarray, return_tlbr: bool = False) -> np.ndarray:
-
-        results = self.face_detection.process(frame)
+        mtcnn_detections = self.detector.detect_faces(frame)
 
         if return_tlbr:
-            if results.detections:
-                return self.tlbr(frame, results.detections)
+            if mtcnn_detections:
+                return self.tlbr(frame, mtcnn_detections)
             return []
 
-        if results.detections:
-            if self.mp_drawing_utils:
-                # Draw face detections of each face using media pipe drawing utils.
-                for detection in results.detections:
-                    self.mp_drawing.draw_detection(frame, detection)
-            
-            else:
-                # Draw face detections of each face using our own tlbr and cv2.rectangle
-                for tlbr in self.tlbr(frame, results.detections):
-                    cv2.rectangle(frame, tlbr[:2][::-1], tlbr[2:][::-1], self.color, self.thickness)
-
+        if mtcnn_detections:
+            if self.draw_detections:
+                for detection in mtcnn_detections:
+                    x, y, width, height = detection['box']
+                    cv2.rectangle(frame, (x, y), (x + width, y + height), self.color, self.thickness)
         return frame
--- main.py
+++ main.py
@@ -1,20 +1,37 @@
-# main.py
+# # main.py
+# from utils import FPSmetric
+# from engine1 import Engine
+# from faceDetection import MPFaceDetection
+# from faceNet.faceNet import FaceNet
+#
+# if __name__ == '__main__':
+#     facenet = FaceNet(
+#         detector = MPFaceDetection(),
+#         onnx_model_path = "models/faceNet.onnx",
+#         anchors = "faces",
+#         force_cpu = True,
+#     )
+
+
+from faceDetection import MTCNNFaceDetection
 from utils import FPSmetric
 from engine1 import Engine
-from faceDetection import MPFaceDetection
 from faceNet.faceNet import FaceNet
 
 if __name__ == '__main__':
     facenet = FaceNet(
-        detector = MPFaceDetection(),
+        detector = MTCNNFaceDetection(),
         onnx_model_path = "models/faceNet.onnx",
         anchors = "faces",
         force_cpu = True,
     )
-    engine = Engine(webcam_id= "rtsp://192.168.0.107:8080/h264_ulaw.sdp", show=True, custom_objects=[facenet, FPSmetric()])
+
+
+    engine = Engine(webcam_id= "rtsp://admin:admin123@192.168.0.150:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif", show=True, custom_objects=[facenet, FPSmetric()])
     #engine = Engine(webcam_id=0, show=True,custom_objects=[facenet, FPSmetric()])
     # save first face crop as anchor, otherwise don't use
     # while not facenet.detect_save_faces(engine.process_webcam(return_frame=True), output_dir="faces"):
      #   continue
 
     engine.run()
+
